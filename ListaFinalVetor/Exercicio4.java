fc0ddc6717c700652cf3c1a571fe01832ba01a8b
Eduardo Franklin <92964687+eduFrn@users.noreply.github.com>
2021-11-25 16:27:02 -0300

    Update Exercicio4.java

--- ListaFinalVetor/Exercicio4.java
+++ ListaFinalVetor/Exercicio4.java
@@ -3,21 +3,18 @@ public class Exercicio4 {
 	public static void main(String[] args) {
 
 		int r, i, j;
-		int[] a = new int[10];
-		
-		for(i=1;i<10;i++) {
+		int[] a = new int[11];
+		a[0]=1;
+		for(i=1;i<11;i++) {
 			r=2;
-			
 			for(j=1;j<i;j++) {
 				r=2*r;
 			}
 			a[i]=r;
 		}
 
-		//apresentação do vetor A
-		System.out.println("Valores:");
-			System.out.println("2 elevado a "+i+" = "+1);
-		for(i=1;i<10;i++) {
+		//apresentaÃ§Ã£o do vetor A
+		for(i=0;i<11;i++) {
 			System.out.println("2 elevado a "+i+" = "+a[i]);
 		}
 
